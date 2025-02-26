b31b93ea6aae279f395cb87cc161dca2f3c67b91
Fandy Hanz <hanzyura28@gmail.com>
2025-02-26 21:40:00 +0700

    add: P3_Percobaan5_12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\UserController;
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
