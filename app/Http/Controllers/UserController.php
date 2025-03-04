d57d095692a5e9dea223654eda7985a29499ac08
Fandy Hanz <hanzyura28@gmail.com>
2025-03-04 22:21:04 +0700

    Add:P4_Percobaan2.1_12

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,15 +9,9 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        
-        $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'manager 3',
-        'password' => Hash::make('12345')
-        ];
-        
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
