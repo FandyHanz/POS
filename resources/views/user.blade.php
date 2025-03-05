9257b80c766bdea5c1fec25607bab7effea85bd6
Fandy Hanz <hanzyura28@gmail.com>
2025-03-05 10:19:27 +0700

    Add:P4_Percobaan2.6_12

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,19 +5,28 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <a href="../public/user/tambah">tambah</a>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
             <td>ID Level Pengguna</td>
+            <td>Aksi</td>
+            <td>tambah</td>
         </tr>
         <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+              ``<td><a href="../public/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="../public/user/hapus/{{$d->user_id}}">Hapus</a></td>
+            </tr>
+            @endforeach
         </tr>
     </table>
+    
 </body>
 </html>
