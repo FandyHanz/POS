746cb322f4fed85df3e0bbc4fa21aabce5e45216
Fandy Hanz <hanzyura28@gmail.com>
2025-06-29 22:38:49 +0700

    Add:change on api and penjualan detail

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\PenjualanDetailController;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
@@ -25,14 +26,15 @@ Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
 Route::post('/logout', LogoutController::class)->name('logout');
 
-Route::get('level', [LevelController::class, 'index']);
-Route::post('level', [LevelController::class, 'store']);
-Route::get('level/{level}', [LevelController::class, 'show']);
-Route::put('level/{level}', [LevelController::class, 'update']);
-Route::delete('level/{level}', [LevelController::class, 'destroy']);
+Route::middleware('auth:api')->group(function () {
+    Route::get('level', [LevelController::class, 'index']);
+    Route::post('level', [LevelController::class, 'store']);
+    Route::get('level/{level}', [LevelController::class, 'show']);
+    Route::delete('level/{level}', [LevelController::class, 'destroy']);
 
 Route::get('user', [UserController::class, 'index']);
 Route::post('user', [UserController::class, 'store']);
+    Route::put('level/{level}', [LevelController::class, 'update']);
 Route::get('user/{user}', [UserController::class, 'show']);
 Route::put('user/{user}', [UserController::class, 'update']);
 Route::delete('user/{user}', [UserController::class, 'destroy']);
@@ -48,6 +50,9 @@ Route::post('barang', [BarangController::class, 'store']);
 Route::get('barang/{barang}', [BarangController::class, 'show']);
 Route::put('barang/{barang}', [BarangController::class, 'update']);
 Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+
+Route::get('detailBarang', [PenjualanDetailController::class, 'show']);
+});
 
 Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
