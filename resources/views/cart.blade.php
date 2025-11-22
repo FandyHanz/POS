e659b75ed3049fca24df4d4af0d630cfed57c0a2
FandyHanz <hanzyura28@gmail.com>
2025-11-22 17:22:32 +0700

    change: reset

--- resources/views/cart.blade.php
+++
@@ -1,7 +0,0 @@
-<html>
-    <body>
-        <h1>cart transaksi</h1>
-        <h2>Nama: {{$username}}</h2>
-        <h2>item: {{$item}}</h2>
-    </body>
-</html>
--- resources/views/penjualan.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>ini adalah bagian dari laman penjualan</h1>
-    </body>
-</html>
